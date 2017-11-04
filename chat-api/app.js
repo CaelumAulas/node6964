ce70e2a00c4e44afcac15dcaaac601aa837131b3
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-04 15:53:34 -0200

    Feito o ajax

--- chat-api/app.js
+++ chat-api/app.js
@@ -3,12 +3,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(cors())
+
 app.post('/msg', (req, res) => {
   const msg = req.body.msg
-  
+
   res.send(msg)
 })
 
